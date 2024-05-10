ae84971b8762f42945db9fadf46e867b34f4c84e
Craig Doremus <cdoremus@gmail.com>
2024-05-10 16:52:53 -0400

    Fixed lifecycle page

--- lifecycle-wc.js
+++ lifecycle-wc.js
@@ -8,22 +8,30 @@ class AttributeChangedWC extends HTMLElement {
     this.add = this.getAttribute("add");
     this.innerHTML = `
       <style>#sum-title{ color: blue;} .sum { color: black; font-weight: bold; }</style>
+      <div>
+      <p id="old-value">Old Attr Value: <span class="old">0</span></p>
+      <p id="new-value">New Attr Value: <span class="new">0</span></p>
+      </div>
       <p id="sum-title">Sum: <span class="sum">0</span></p>
     `;
   }
   attributeChangedCallback(attrName, oldValue, newValue) {
-    if (attrName === "add") { 
-      console.log(`oldValue: ${oldValue}, newValue: ${newValue}`);
+    if (attrName === "add") {
+      console.log(`Old add Value: ${oldValue}, New add Value: ${newValue}`);
       let sum = 1;
       const sumDisplay = this.querySelector(".sum");
+      const oldVal = this.querySelector(".old");
+      const newVal = this.querySelector(".new");
       console.log("sumDisplay: ", sumDisplay);
-      if (oldValue !== null) {
+      if (oldValue) {
+        oldVal.innerHTML = oldValue;
+        newVal.innerHTML = newValue;
         sum = parseInt(oldValue) + parseInt(newValue);
       } else {
         sum = parseInt(newValue);
       }
       console.log("sum: ", sum);
-      if (sum.sumDisplay !== null) {
+      if (sum !== null && sumDisplay !== null) {
         sumDisplay.innerHTML = sum;
       }
     }
